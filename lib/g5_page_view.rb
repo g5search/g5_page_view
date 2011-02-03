259a96dfd72e69708a3426b37bac4f1d88ce73f9
Tim Linquist <timo3377@gmail.com>
2011-02-02 21:53:40 -0800

    Reworked connection/db logic

--- lib/g5_page_view.rb
+++ lib/g5_page_view.rb
@@ -1,20 +1,24 @@
 require 'mongo'
 require 'bson'
 
+require 'ruby-debug'
+
 module G5PageView
   class << self
     attr_reader :connection, :db, :config
 
-    def db
-      @db ||= self.connection[self.config['database']]
-    end
-
     def connect!(config={})
-      @config ||= config
-      options = config.inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo}
+      options = Marshal.load(Marshal.dump(config.inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo}))      
       nodes= options.delete(:nodes)
       raise "Invalid nodes specified" unless nodes && nodes.respond_to?(:<<)
       @connection= Mongo::ReplSetConnection.new(*(nodes << options))
+      configure!(config)
+    end
+    
+    def configure!(config={})
+      @config = config
+      raise 'No database specified' unless @config.has_key?('database')
+      @db = @connection[@config['database']] 
     end
   end
 end
--- spec/g5_page_view_spec.rb
+++ spec/g5_page_view_spec.rb
@@ -7,7 +7,16 @@ describe G5PageView do
   
   describe "Connection" do
     describe "Database" do
+      before(:each) do
+        @config= {"nodes"=>[["localhost", "27017"]], 'database'=>'gts_test'}
+      end
+      it "raise an error if no database is specified" do
+        @config.delete('database')
+        lambda{ G5PageView::connect!(@config) }.should raise_error('No database specified')        
+      end
+      
       it "gets the database when connected" do
+        G5PageView::connect!(@config)
         db= G5PageView::db
         db.name.should eql(G5PageView::config['database'])
       end
@@ -21,15 +30,13 @@ describe G5PageView do
       lambda{ G5PageView::connect!({:nodes=>{}}) }.should raise_error('Invalid nodes specified')      
     end
     
-    it "should connect to the replica set" do
-      config= {"test"=>{"nodes"=>[["localhost", "27017"]]}}
-      G5PageView::connect!(config['test'])
+    it "should be connected to the replica set" do
       G5PageView::connection.should be_connected
     end
     
     it "should connect with the symbolized configs" do
-      config= {"test"=>{"nodes"=>[["localhost", "27017"]], "rs_name"=>'gts_replica_test'}}
-      Mongo::ReplSetConnection.should_receive(:new).with(["localhost", "27017"], {:rs_name=>'gts_replica_test'}).and_return(@real_connection)
+      config= {"test"=>{"nodes"=>[["localhost", "27017"]], "rs_name"=>'gts_replica_test', 'database'=>'gts_test'}}
+      Mongo::ReplSetConnection.should_receive(:new).with(["localhost", "27017"], {:rs_name=>'gts_replica_test', :database=>'gts_test'}).and_return(@real_connection)
       G5PageView::connect!(config['test'])
     end
   end
